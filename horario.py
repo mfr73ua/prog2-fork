3b8b26a4ae79a1a4e9abbd4265441aa50ef61bd1
ra-and5 <ram58@alu.ua.es>
2025-02-24 14:04:18 +0100

    se añade asignatura

--- horario.py
+++ horario.py
@@ -1,9 +1,10 @@
 class Horario:
     total_horarios = 0  # Contador de instancias
 
-    def __init__(self, curso, dia, hora_inicio, hora_fin):
+    def __init__(self, curso, dia, hora_inicio, hora_fin, asignatura):
         self.curso = curso
         self.dia = dia
         self.hora_inicio = hora_inicio
         self.hora_fin = hora_fin
+        self.asignatura = asignatura
         Horario.total_horarios += 1  # Aumenta el contador cada vez que se crea un horario
